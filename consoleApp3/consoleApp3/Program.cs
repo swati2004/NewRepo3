40b840ae5f2d9066da83c3527be1c7e8466a556a
gargs <gargs@AD0315-NN7LFCM>
2021-09-28 09:21:47 +0530

    code change

--- consoleApp3/consoleApp3/Program.cs
+++ consoleApp3/consoleApp3/Program.cs
@@ -10,8 +10,57 @@ namespace consoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello123");
-            Console.WriteLine("hh23");
+            Console.WriteLine("Enter player name: ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Enter player Team name: ");
+            String teamname = Console.ReadLine();
+            Console.WriteLine("Enter no of matches: ");
+            int noOfMatches = int.Parse(Console.ReadLine());
+
+            long totalRaidPoints = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter total Defence points: ");
+            long totalDefencePoints = long.Parse(Console.ReadLine());
+            IKabbadiPlayerStatistics kabbadiPlayerStatistics = new Student(name, teamname, noOfMatches, totalRaidPoints, totalRaidPoints);
+            kabbadiPlayerStatistics.DisplayKabbadiPlayerDetails();
+        }
+        interface IKabbadiPlayerStatistics
+        {
+            void DisplayKabbadiPlayerDetails();
+        }
+        class Student : IKabbadiPlayerStatistics
+        {
+            String name;
+            String teamname;
+            int noOfMatches;
+            long totalRaidPoints;
+            long totalDefencePoints;
+            string teamMembers;
+            int NoOfTeamMembers;
+            float noOfBalls;
+
+
+
+            public Student(String name, String teamname, int noOfMatches, long totalRaidPoints, long totalDefencePoints)
+            {
+                this.name = name;
+                this.teamname = teamname;
+                this.noOfMatches = noOfMatches;
+                this.totalRaidPoints = totalRaidPoints;
+                this.totalDefencePoints = totalDefencePoints;
+            }
+            public void DisplayKabbadiPlayerDetails()
+            {
+                Console.WriteLine("Player Details");
+                Console.WriteLine("Player name: " + name);
+                Console.WriteLine("Team name: " + teamname);
+                Console.WriteLine("No of matches: " + noOfMatches);
+                Console.WriteLine("Total raid points: " + totalRaidPoints);
+                Console.WriteLine("Total defence points: " + totalDefencePoints);
+
+
+
+            }
+
         }
     }
 }
